a2ba5df06b956c5c1453fbd9070a17e72aed4492
felipe <pipe.verri@gmail.com>
2025-09-05 17:24:24 -0300

    Biome implementado

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { ReactNode } from "react";
 
-export default function RootLayout({children}: {children: ReactNode}) {
-    return (
-        <html lang="en">
-            <body>
-                <h1>hola</h1>
-                {children}
-            </body>
-        </html>
-    )
+/* Voy a usar el router de NextJS, donde layout.tsx es el wrapper alrededor de todos los hijos y cada ruta es un folder */
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
