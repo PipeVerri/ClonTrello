084fc81ea874874e89b2e80037018b67d90490d2
felipe <pipe.verri@gmail.com>
2025-09-09 18:55:29 -0300

    Basic backend requests structure

---
+++ packages/db/src/index.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "./generated/prisma/client";
+
+export const prisma = new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"]
+})
+
+export async function disconnect(): Promise<void> {
+    await prisma.$disconnect()
+}
