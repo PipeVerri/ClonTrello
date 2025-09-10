3c2750589ce71b2777f5c0c2c14a7c74c2b180a4
felipe <pipe.verri@gmail.com>
2025-09-10 13:29:08 -0300

    pino logging

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,11 +3,13 @@ import boards from "./routes/board.route"
 import * as dotenv from "dotenv"
 import {errorHandler} from "./middleware/errorHandler";
 dotenv.config()
+import {pinoHttp} from "pino-http";
 
 export function createServer() {
     const app = express()
     // Middleware
     app.use(express.json())
+    app.use(pinoHttp())
     // Rutas
     app.use("/boards", boards)
     // El error handler al final, para agarrar todo
